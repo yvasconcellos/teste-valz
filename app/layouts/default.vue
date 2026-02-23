aee64b6aeed117a2c11f7c5a34f94d0ac4bd23f7
Yuri Vasconcellos <y.vasconcellos@outlook.com>
2026-02-23 19:05:43 -0300

    feat: adiciona layout default para página de login

---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="min-h-screen flex flex-col bg-gray-50 text-gray-900">
+    <AppHeader />
+
+    <main class="flex-1">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
