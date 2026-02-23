d51d2324984ea0e51cd2f9cae57d201e2838bda6
Yuri Vasconcellos <y.vasconcellos@outlook.com>
2026-02-23 19:04:44 -0300

    feat: criação de barra de progresso

---
+++ app/components/ProgressBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full bg-gray-200 rounded h-2">
+    <div class="bg-blue-600 h-2 rounded" :style="{ width: `${value}%` }" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{ value: number }>()
+</script>
